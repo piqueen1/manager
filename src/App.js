c74915dd41f06572b1c5c60ba4d76979063b0e94
Finn Thye <piqueen@gmail.com>
2016-12-28 15:29:07 -0700

    Following the Udemy tutorial...

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAnUtG9KMKwk6Cr1kBQVx6R0aZgSPgvgUs',
+      authDomain: 'manager-c2639.firebaseapp.com',
+      databaseURL: 'https://manager-c2639.firebaseio.com',
+      storageBucket: 'manager-c2639.appspot.com',
+      messagingSenderId: '1089625461995'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
